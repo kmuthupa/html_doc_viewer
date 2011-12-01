e06412984cffc893e43b7b17fb6c4fd54548bd7c
Karthik Muthupalaniappan <karthik.swaminathan@me.com>
2011-12-01 15:25:23 +0530

    more work

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,9 +4,15 @@ class DocumentsController < ApplicationController
   
   def upload
     if params[:document_file] && File.exists?(params[:document_file].tempfile)
+      # persist the file
+      file_name = params[:document_file].original_filename
+      save_file = File.new("#{Rails.root}/tmp/uploads/#{file_name}", 'w')
       File.open(params[:document_file].tempfile, 'r') do |f|
-        file_content = f.read
+        save_file.write(f.read)
       end
+      save_file.close
+      
+      # upload the file to conversion service
     end
     respond_to do |format|
       format.html {render :action => 'result'}
