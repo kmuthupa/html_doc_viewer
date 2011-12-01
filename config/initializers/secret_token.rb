015e94f9786d4382b2345b4db0b2bdd7b211117e
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2011-12-01 10:04:04 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlDocViewer::Application.config.secret_token = 'fb748d920b0848119ba404b3bbd1f4e52be901809bbb3410b293f75cd16eb3d4319d83019f1ec7974afba538b827ae896f351c98518e517b7a5c4c7698545804'
