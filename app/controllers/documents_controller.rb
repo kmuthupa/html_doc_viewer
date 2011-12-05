7194b844f1c9e0bb3b579a6625683895e34243ca
Karthik Muthupalaniappan <karthik.swaminathan@me.com>
2011-12-05 14:11:27 +0530

    fix issue with upload/processing of files with spaced file names

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -13,7 +13,7 @@ class DocumentsController < ApplicationController
         save_file.write(f.read)
       end
       save_file.close
-      save_file_name = File.basename(save_file.path)
+      save_file_name = File.basename(save_file.path).gsub(/\s+/, "")
       # upload the file to the sinatra conversion service
       begin
         @conversion_response = RestClient.post "#{CONVERSION_SERVER}/#{save_file_name}", :data => File.new("#{save_file.path}")       
@@ -22,8 +22,8 @@ class DocumentsController < ApplicationController
           #@html_source = @conversion_response.body if @conversion_response.respond_to?(:body)
           @link_to_html = @conversion_response.body.gsub('.pdf', '.html') if @conversion_response.respond_to?(:body)
         end
-      rescue
-        raise 'conversion service is unavailable at the moment!'
+      rescue Exception => ex
+        raise "The conversion service is unavailable or not responding at the moment! Exception info: #{ex.message} #{ex.backtrace.inspect}"
       end
       respond_to do |format|
         format.html {render 'result'}
--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -51,6 +51,24 @@ describe DocumentsController do
       assigns(:doc_name).should be_nil
       assigns(:conversion_response).code.should == 500
     end
+    
+    describe 'upload of file with spaced file name' do
+      before(:each) do
+        @test_document = fixture_file_upload('/Sivasankari Ranganathan.pdf', 'application/pdf')
+        class << @test_document #hack to simulate the file upload action
+           attr_reader :tempfile
+        end
+      end
+      
+       it 'should upload a file to the server with success and process it successfully' do
+          RestClient.stub!(:post).and_return(StubResponse.new(200)) if STUB_CONVERSION
+          post :upload, {:document_file => @test_document}
+          response.should be_success
+          response.should render_template(:result)
+          assigns(:doc_name).should == 'SivasankariRanganathan.pdf'
+          assigns(:conversion_response).code.should == 200
+        end
+    end
   end
 end
 
