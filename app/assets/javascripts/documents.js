6bbbb4bd496a21477d7c765532f8f7cc43bcd05a
Karthik Muthupalaniappan <karthik.m@imaginea.com>
2011-12-15 14:40:33 +0530

    refactor page navigation for the viewer

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {	
-  if ($('#page_link_1') != undefined) {
-	url = $('#page_link_1').attr('value');
-	loadPage(url);
+  // load the first page for the document on view load
+  if ($('#page_select option:first') != undefined) {
+	url = $('#page_select option:first').attr('value');
+	if (url != undefined) {
+	  loadPage(url);
+    }
   }
 
   $('#page_select').change(function() {
@@ -10,16 +13,28 @@ $(document).ready(function() {
   });
 
   $('#prev_page').click(function() {
-	
-	
+	prev = $("#page_select option:selected").prev('option');
+	url = prev.attr('value');
+    loadPageAndSetSelect(url);
   });
 
   $('#next_page').click(function() {
-	
-	
+	next = $("#page_select option:selected").next('option');
+	url = next.attr('value');
+    loadPageAndSetSelect(url);
   }); 
 });
 
+
+// loads the page content and sets the page select list
+var loadPageAndSetSelect = function(url_val) {
+	if (url_val != undefined) {
+		$('#page_select').val(url_val);
+		loadPage(url_val);
+	};
+}
+
+// loads the content for the specific page using the passed url
 var loadPage = function (page_url) {
 	$('#spinner').show();
 	$('#document').html('');
